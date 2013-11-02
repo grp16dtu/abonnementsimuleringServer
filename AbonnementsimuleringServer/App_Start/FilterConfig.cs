5325bb40161f9393aef926c3fa8ee62fa6b2097f
Lasse Dalgaard <lasse@redlaz.dk>
2013-11-02 20:47:54 +0100

    WEB API

---
+++ AbonnementsimuleringServer/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AbonnementsimuleringServer
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
