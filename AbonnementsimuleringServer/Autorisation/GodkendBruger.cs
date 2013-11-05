abc2f1ea2b33dbb6951754a1b0b2de9582f8ef44
redlaz <lasse@redlaz.dk>
2013-11-05 21:36:10 +0100

    Lidt basic auth sikkerhed

---
+++ AbonnementsimuleringServer/Autorisation/GodkendBruger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApiContrib.MessageHandlers;
+
+namespace AbonnementsimuleringServer.Autorisation
+{
+    public class MyBasicAuthenticationHandler : BasicAuthenticationHandler
+    {
+        protected override bool Authorize(string username, string password)
+        {
+            if (username == "myusername" && password == "mypassword") return true;  
+                return false;
+        }
+
+        protected override string Realm
+        {
+            get { return "Abonnement simulering"; }
+        }
+    }
+}
--- AbonnementsimuleringServer/Global.asax.cs
+++ AbonnementsimuleringServer/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AbonnementsimuleringServer.Autorisation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,10 @@ namespace AbonnementsimuleringServer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // AUTH
+            //GlobalConfiguration.Configuration.MessageHandlers.Add(new MyBasicAuthenticationHandler());
+ 
         }
     }
 }
