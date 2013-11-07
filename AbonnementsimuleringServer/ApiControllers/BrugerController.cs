23b6f09aea64305a715f925f6e606fdd7f8c1296
redlaz <lasse@redlaz.dk>
2013-11-07 15:34:04 +0100

    Actions routing OK

--- AbonnementsimuleringServer/ApiControllers/BrugerController.cs
+++ AbonnementsimuleringServer/ApiControllers/BrugerController.cs
@@ -11,6 +11,7 @@ namespace AbonnementsimuleringServer.ApiControllers
     public class BrugerController : ApiController
     {
         [HttpGet]
+        [ActionName("Hent")]
         public HttpResponseMessage Hent(string brugernavn, string kodeord)
         {
 
@@ -23,5 +24,28 @@ namespace AbonnementsimuleringServer.ApiControllers
 
             return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Bruger ikke fundet");
         }
+
+        [HttpGet]
+        [ActionName("Opret")]
+        public HttpResponseMessage Opret(Bruger bruger)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "FEJL Opret");
+        }
+
+        [HttpGet]
+        [ActionName("Rediger")]
+        public HttpResponseMessage Rediger(Bruger bruger)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Rediger");
+        }
+
+        [HttpGet]
+        [ActionName("Slet")]
+        public HttpResponseMessage Slet(Bruger bruger)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "slet");
+        }
+
+
     }
 }
--- AbonnementsimuleringServer/App_Start/RouteConfig.cs
+++ AbonnementsimuleringServer/App_Start/RouteConfig.cs
@@ -18,17 +18,7 @@ namespace AbonnementsimuleringServer
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
-                name: "Anden",
-                url: "api/{controller}/{action}/{id}",
-                defaults: new { }
-            );
-
-            
-
-  
+            );      
         }
     }
 }
